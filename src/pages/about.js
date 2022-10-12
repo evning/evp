95e8973eb4f71b306d58e56ee5cb0ccc727eb767
evning <ev_padilla@outlook.com>
2022-10-12 09:40:06 -0400

    Update about.js

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>My About Page</h1>
+      <p>This is my About page</p>
+    </div>
+  )
+}
+export default About
