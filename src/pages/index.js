01749ebb7df1053e34a8e730af233278ca539f83
evning <ev_padilla@outlook.com>
2022-10-12 09:42:07 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const Index = () => {
   return (
     <div>
+      <Link to="/about"></Link>
       <h1>My Landing Page</h1>
       <p>This is my landing page</p>
     </div>
