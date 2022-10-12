bf5fa52053328a9bdf6c19dc4adf30d976976da0
evning <ev_padilla@outlook.com>
2022-10-12 09:44:26 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import * as React from 'react'
 const About = () => {
   return (
     <div>
+        <Link to="/">Home Page</Link>
       <h1>My About Page</h1>
       <p>This is my About page</p>
     </div>
