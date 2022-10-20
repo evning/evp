f8364e0474da95d3e8d7ebd2bacfb69bfa80a333
evning <ev_padilla@outlook.com>
2022-10-20 13:34:58 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+
 import "../components/index.css"
 import { navigate } from "gatsby"
 
@@ -10,7 +10,7 @@ const Index = () => {
         <div className='heroInfo'>
         <h1>Jeff C. Salter</h1>
         <p>Cybersecurity and Information Design</p>
-        <button className='heroButton' onClick={()=>{navigate("/about")}}>How We Can Help</button>
+      
       </div>
     </div>
   </header>
