3f69a98fd959c0f96c7b0da10b12dcc9086206fd
evning <ev_padilla@outlook.com>
2022-11-06 16:57:27 -0500

    argh

---
+++ src/pages/gallery.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const gallery =  () => {
+    return(
+        <div>gallery</div>
+    )
+}
+
+export default gallery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import "../components/index.css"
 import { navigate } from "gatsby"
+import { Link } from 'gatsby';
+/*
+import React from 'react';
+import { Link } from "gatsby"
 import "../components/index.css"
-import { Link } from 'gatsby'
+*/
+
 const Index = () => {
   return (
     <header className='hero'>
@@ -10,7 +15,7 @@ const Index = () => {
         <div className='heroInfo'>
         <h1>Jeff C. Salter</h1>
         <p>Cybersecurity and Information Design</p>
-      
+        <button className='heroButton' onClick={()=>{navigate("/about'")}}>How We Can Help</button>
       </div>
     </div>
   </header>
