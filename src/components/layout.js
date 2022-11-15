a90b7b80c1e2a353a3a6353db07a2ccc2cb4351e
evning <ev_padilla@outlook.com>
2022-11-14 20:06:13 -0500

    wheee

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "./header"
+import BottomNav from './bottomNav'
 
 const Layout = props => {
     return(
         <div>
             <Header />
             {props.children}
+            <BottomNav />
         </div>
     )
 }
