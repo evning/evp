0a437b3fd57513ac79311613f09f02a55ec7f840
evning <ev_padilla@outlook.com>
2022-12-04 18:38:17 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from "../components/layout"
 import Card from ".../components/card"
-import /*cardGroup from*/ "../components/index.css"
+import cardGroup from "../components/index.css"
 import { Link } from "gatsby"
 
 const Index = () => {
