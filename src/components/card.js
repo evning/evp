f5cdb07e8899265551ce552493bef47f50b433ee
evning <ev_padilla@outlook.com>
2022-12-03 20:53:25 -0500

    fhdjskf

---
+++ src/components/card.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Card = props => {
+    <div classname="Card">
+        <h3>Web Design</h3>
+        <p>Sub Title</p>
+    </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Layout from "../components/layout"
+import Card from ".../components/card"
 
 const Index = () => {
 
   return(
     <Layout>
-      <h1>This is the homepagebody copy area</h1>
+      <div className="Cards">
+        <h2>How can we help</h2>
+        <Card 
+          title="Web Design"
+          text="Sub Title"/>
+          <Card 
+          title="Custom Photography"
+          text="Location or Studio"/>
+          <Card 
+          title="Ideas and Recommendations"
+          text="Let us build up your web presence "/>
+      </div>
     </Layout>
   )
 }
+
 export default Index
 
 
