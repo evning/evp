bc0430a49f3f5e75483a9653db835362e3146404
evning <ev_padilla@outlook.com>
2022-12-04 16:11:46 -0500

    ALMSOT DONE!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import Layout from "../components/layout"
 import Card from ".../components/card"
+import /*cardGroup from*/ "../components/index.css"
+import { Link } from "gatsby"
 
 const Index = () => {
-
   return(
     <Layout>
       <div className="Cards">
         <h2>How can we help</h2>
         <div className="cardGroup">
-        <Card 
-          title="Web Design"
-          text="Sub Title"/>
-          <Card 
-          title="Custom Photography"
-          text="Location or Studio"/>
-          <Card 
-          title="Ideas and Recommendations"
-          text="Let us build up your web presence "/>
+          <div className="cardOne">
+            <Link to ="/services">
+            <Card 
+              title="Web Design"
+              text="Sub Title"
+           />
+           </Link>
+          </div>
+          <div className="cardTwo">
+            <Link to ="/contact">
+            <Card 
+              title="Custom Photography"
+              text="Location or Studio"
+           />
+           </Link>
+          </div>
+          <div className="cardThree">
+            <Link to ="/gallery">
+            <Card 
+              title="Ideas and Recommendations"
+              text="Let us build up your web presence"
+           />
+           </Link>
+          </div> 
+         </div>
       </div>
-    </div>
     </Layout>
   )
 }
