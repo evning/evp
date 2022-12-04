aa18caa8272dc0cb56114e4f2cbf97748fd170a9
evning <ev_padilla@outlook.com>
2022-12-04 18:00:39 -0500

    YE

--- src/components/bottomNav.js
+++ src/components/bottomNav.js
@@ -1,19 +1,1 @@
-import React from 'react'
-import { Link } from "gatsby"
-import * as bottomStyles from "../components/bottomnav.module.css"
-
-const BottomNav = () => {
-    return (
-        <div className={bottomStyles.navWrapper} >
-            <div className={bottomStyles.mainNav}>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/gallery">Gallery</Link>
-            </div>
         </div>
-    )
-}
-
-export default BottomNav
--- src/components/footer.js
+++ src/components/footer.js
@@ -5,5 +5,7 @@ import * as footerStyles from "./footer.module.css"
 const Footer = () => {
     const year = new Date().getFullyYear();
 
-    return <p className={footerStyles.footer}>{'Copyright c Jeff Salter ${year}'}</p>
+    return <p className={footerStyles.footer}>{'Copyright © Evelyn Padilla ${year}'}</p>;
 };
+
+export default Footer;
