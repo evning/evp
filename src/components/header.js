cde27e54541e245baf26bcae096203f06dc311d7
evning <ev_padilla@outlook.com>
2022-12-04 18:40:16 -0500

    ger

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 
 import * as heroStyles from "./hero.module.css"
 import { navigate } from "gatsby"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import Layout from "../components/layout"
 import Card from ".../components/card"
 import cardGroup from "../components/index.css"
