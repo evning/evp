cea6788986ad46b9f5e73b3b942d5cfd34bd44ce
evning <ev_padilla@outlook.com>
2022-12-04 19:32:21 -0500

    WHAT

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from "../components/layout"
-import Card from ".../components/card"
+import Card from "../components/card"
 import "../components/index.css"
 import { Link } from "gatsby"
 
