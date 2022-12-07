97cc16b7f6029a6541950b72628e8cd9f5ea2382
evning <ev_padilla@outlook.com>
2022-12-07 11:38:23 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from "../components/layout"
-import Card from "../components/card"
+import Cards from "../components/card"
 import "../components/index.css"
 import { Link } from "gatsby"
 
